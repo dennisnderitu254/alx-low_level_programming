a90c466ead7cc6493241c7f285c5b90e0f3ab783
dennisnderitu254 <dknderitu@gmail.com>
2022-09-08 10:45:12 -0700

    Hello , puts - making changes to coding layout by Betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
- * * main - Write '"Programming is like building a multilingual puzzle'
- * *Return: Always 0 (success)
- * */
+ * main - Entry point
+ *   
+ * Return: Alway 0 (Success)
+ *     
+ **/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+		return (0);
 }

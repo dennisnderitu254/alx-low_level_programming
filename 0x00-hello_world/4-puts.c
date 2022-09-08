b384db1e5421b173e3d0b235e54cdd0fa980765f
dennisnderitu254 <dknderitu@gmail.com>
2022-09-08 10:31:35 -0700

    C program that prints exactly Programming is like building a multilingual puzzle, followed by a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * *main - Main function of my project
+ * *
+ * *
+ * *Return: return 0 everytime
+ * */
+int main(void)
+{
+
+		char displayed[52];
+
+			strcpy(displayed, "\"Programming is like building a multilingual puzzle");
+				puts(displayed);
+					return (0);
+}

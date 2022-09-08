c476fd35ecbca16c5dc5f2a767f7d659b8836d07
dennisnderitu254 <dknderitu@gmail.com>
2022-09-08 10:40:09 -0700

    Hello, puts - making hanges on the code layout with betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
-#include <string.h>
-
-/*
- * *main - Main function of my project
- * *
- * *
- * *Return: return 0 everytime
-  */
+/**
+ * * main - Write '"Programming is like building a multilingual puzzle'
+ * *Return: Always 0 (success)
+ * */
 int main(void)
 {
-
-		char displayed[52];
-
-			strcpy(displayed, "\"Programming is like building a multilingual puzzle");
-				puts(displayed);
-					return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }

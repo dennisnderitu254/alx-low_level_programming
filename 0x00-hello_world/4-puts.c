b273ad32cd33041bc07901efd96f41f485bed21a
dennisnderitu254 <dknderitu@gmail.com>
2022-09-08 10:51:35 -0700

    Hello, puts problem - chamging coding layout

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *   
- * Return: Alway 0 (Success)
- *     
- **/
+ *
+ * Return: Always  0 (success)
+ *
+ */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
 }

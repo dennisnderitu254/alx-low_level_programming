a603d64e60fb97deba08f3a09483946fee91be28
dennisnderitu254 <dknderitu@gmail.com>
2022-09-09 10:33:13 -0700

    making changes to for loop

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int digit1, digit2;
 
-	for (digit2 = digit1 < 9; digit1++ ;)
+	for (digit1 = 0; digit1 < 9; digit1++)
 	{
 		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{

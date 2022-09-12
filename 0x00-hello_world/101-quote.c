6844fd6a4a33ff396258e57e8724b8078a9d01d8
dennisnderitu254 <dknderitu@gmail.com>
2022-09-11 22:09:08 -0700

    C program that prints exactly and that piece of art is useful - Dora Korpar, 2015-10-19

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Event point
+ * main - main function in the task
  *
- * Return: Always 0 (Success)
+ *
+ * return: return 1
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
-
-		return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }

705514534659b38863922db9fe191af1506d6ec5
dennisnderitu254 <dknderitu@gmail.com>
2022-09-14 03:56:42 -0700

    Removing whitespaces using betty linter

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,18 +7,16 @@
 
 int main(void)
 {
-        int i;
+	int i;
 
-        printf("Infinite loop incoming :(\n");
-
-        i = 0;
+	printf("Infinite loop incoming :(\n");
+	i = 0;
 	/*
-        while (i < 10)
-        {
-                putchar(i);
-        }
+	 * while (i < 10)
+	{
+		putchar(i);
+	}
 	*/
-        printf("Infinite loop avoided! \\o/\n");
-
-        return (0);
+	printf("Infinite loop avoided! \\o/\n");
+	return (0);
 }

0a967410b64703e65b23e2a37591745119ee0380
dennisnderitu254 <dknderitu@gmail.com>
2022-09-14 03:59:59 -0700

    adding lines in code

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,6 +10,7 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
+
 	i = 0;
 	/*
 	 * while (i < 10)
@@ -17,6 +18,8 @@ int main(void)
 		putchar(i);
 	}
 	*/
+
 	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }

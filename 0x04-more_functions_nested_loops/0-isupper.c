837db938624cb9b96b0977b4c5a7ae2cd8c9006d
dennisnderitu254 <dknderitu@gmail.com>
2022-09-15 02:30:46 -0700

    removing white spaces

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,10 +5,10 @@
  *Return: returns 1 if it is uppercase, 0 if not
  */
 
- int _isupper(int c)
- {
- if ((c >= 'A') && (c <= 'Z'))
- return (1);
+int _isupper(int c)
+{
+if ((c >= 'A') && (c <= 'Z'))
+return (1);
 
- return (0);
- }
+return (0);
+}

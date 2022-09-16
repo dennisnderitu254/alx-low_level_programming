d170fbca2178fb3e1e3e3576237a39d80c82485b
dennisnderitu254 <dknderitu@gmail.com>
2022-09-15 23:52:42 -0700

    the fizz buzz problem

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,18 +16,18 @@ int main(void)
 	printf("FizzBuzz");
 
 	else if ((num % 3) == 0)
-	printf("Fizz");
+	printf("Fizz ");
 
 	else if ((num % 5) == 0)
 	{
 	if (num != 100)
-	printf("Buzz");
+	printf("Buzz ");
 
 	else
 	printf("Buzz");
 	}
 	else
-	printf("%d", num);
+	printf("%d ", num);
 
 	printf("\n");
 	return (0);

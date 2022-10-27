261f0b90e117c1da6c0c5de600c4b82b8da16945
dennisnderitu254 <dknderitu@gmail.com>
2022-10-27 14:59:54 +0300

    function that prints all the elements of a listint_t list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,21 +1,34 @@
-#include "lists.h"
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * print_listint - prints all the elements of a linked list
- * @h: head of the list
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
  *
- * Return: the number of nodes
+ * Description: singly linked list node structure
+ * 
  */
-size_t print_listint(const listint_t *h)
+typedef struct listint_s
 {
-const listint_t *cursor = h;
-size_t count = 0;
+int n;
+struct listint_s *next;
+} listint_t;
 
-while (cursor != NULL)
-{
-printf("%d\n", cursor->n);
-count += 1;
-cursor = cursor->next;
-}
-return (count);
-}
+
+size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+void free_listint2(listint_t **head);
+int pop_listint(listint_t **head);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+int sum_listint(listint_t *head);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+
+#endif

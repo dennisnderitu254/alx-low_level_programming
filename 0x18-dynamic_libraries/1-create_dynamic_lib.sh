ce36069e59086c88ae790b9954c05758b3895192
dennisnderitu254 <dknderitu@gmail.com>
2022-12-13 12:30:15 +0300

    script that creates a dynamic library

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/bash
-gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c 
-gcc -shared -o liball.so *.c
-export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+#!/bin/bash
+gcc -c -fPIC *.c
+gcc -shared -o liball.so *.o

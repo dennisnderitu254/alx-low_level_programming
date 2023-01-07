5dac13316497100f90c0cf0af1f2542763438d7a
dennisnderitu254 <dknderitu@gmail.com>
2023-01-07 22:18:47 +0300

    betty linter corretion

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,20 +7,21 @@
  * Return: If an error occurs - NULL.
  *         Otherwise - a pointer to the new hash table.
  */
-hash_table_t *hash_table_create(unsigned long int size){
-    hash_table_t *ht;
-    unsigned long int i;
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	unsigned long int i;
 
-    ht = malloc(sizeof(hash_table_t));
-    if (ht == NULL)
-        return (NULL);
-    
-    ht->size = size;
-    ht->array = malloc(sizeof(hash_node_t *)* size);
-    if (ht->array == NULL)
-            return (NULL);
-    for (i = 0; i < size; i++)
-            ht->array[i] = NULL;
-    
-    return (ht);
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
+
+	return (ht);
 }

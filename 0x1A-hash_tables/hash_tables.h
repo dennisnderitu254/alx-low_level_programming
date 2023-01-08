a183c69cabace7e441aa9aa9c5aec259d018158f
dennisnderitu254 <dknderitu@gmail.com>
2023-01-08 17:50:47 +0300

    7. ['Betty'] = 'Cool'

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -79,6 +79,11 @@ typedef struct shash_table_s
      shash_node_t *stail;
 } shash_table_t;
 
-hash_table_t *hash_table_create(unsigned long int size);
+shash_table_t *shash_table_create(unsigned long int size);
+int shash_table_set(shash_table_t *ht, const char *key, const char *value);
+char *shash_table_get(const shash_table_t *ht, const char *key);
+void shash_table_print(const shash_table_t *ht);
+void shash_table_print_rev(const shash_table_t *ht);
+void shash_table_delete(shash_table_t *ht);
 
 #endif /* HASH_TABLES */

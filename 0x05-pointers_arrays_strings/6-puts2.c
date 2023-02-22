f80b6fb81010b713d2c1e6257f0512b14ebcb7dd
dennisnderitu254 <dknderitu@gmail.com>
2023-02-22 11:34:15 +0300

    adding code functionality and Time Complexity

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,6 +7,18 @@
  * Return: void
  **/
 
+/*
+1. Declare a function called puts2 that takes a string as an argument.
+2. Declare an integer variable called i and initialize it to 0.
+3. Create a while loop that will iterate through the string until it reaches the null character.
+4. Inside the while loop, create an if statement that checks if the value of i is divisible by 2.
+5. If the value of i is divisible by 2, print the character at the index of i.
+6. Increment i by 1.
+7. After the while loop, print a new line.
+*/
+/*
+Time Complexity: O(n)
+*/
 void puts2(char *str)
 {
 	int i = 0;
@@ -14,7 +26,7 @@ void puts2(char *str)
 	while (str[i] != '\0')
 	{
 		if (i % 2 == 0)
-		_putchar(str[i]);
+			_putchar(str[i]);
 		i++;
 	}
 	_putchar('\n');

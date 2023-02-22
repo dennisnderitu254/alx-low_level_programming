04d77bbb21edfad2368e541bb6f1d0baf88c9c40
dennisnderitu254 <dknderitu@gmail.com>
2023-02-22 16:13:06 +0300

    adding code functionality and time complexity

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,6 +9,16 @@
  * Return: returns pointer to @dest
  */
 
+/*
+1. First, we’re declaring a function called _strcat that takes two arguments: dest and src.
+2.  Next, we’re declaring two variables: index and dest_len.
+3.  Then, we’re using a while loop to iterate through the dest string and find its length.
+4.  Next, we’re using a for loop to iterate through the src string and append each character to the end of the dest string.
+5.  Finally, we’re returning the dest string.
+*/
+/*
+Time Complexity: O(n)
+*/
 char *_strcat(char *dest, char *src)
 {
 	int index = 0, dest_len = 0;

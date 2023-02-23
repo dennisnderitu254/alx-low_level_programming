7f1e26dca9b450d1876f5dc9ffe08c00dafb23c9
dennisnderitu254 <dknderitu@gmail.com>
2023-02-23 11:56:00 +0300

    5. Island Perimeter - Create a function def island_perimeter(grid): that returns the perimeter of the island described in grid

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Defines an island perimeter measuring function."""
+
+
+def island_perimeter(grid):
+    """Return the perimiter of an island.
+    The grid represents water by 0 and land by 1.
+    Args:
+        grid (list): A list of list of integers representing an island.
+    Returns:
+        The perimeter of the island defined in grid.
+    """
+    # 1. We first find the width and height of the grid.
+    # 2. We then iterate through the grid and find the number of edges and the number of nodes.
+    # 3. We then return the number of nodes * 4 - number of edges * 2.
+    # Time Complexity: O(n^2)
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2

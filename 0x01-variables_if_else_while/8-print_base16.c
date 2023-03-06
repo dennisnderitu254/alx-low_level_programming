c250521e289f86dd67eb46a81038e682b7e455cb
dennisnderitu254 <dknderitu@gmail.com>
2023-03-06 19:49:54 +0300

    adding code functionality and time complexity

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,6 +4,19 @@
  * main - Print all single digits of base 16 followed by new line
  *
  * Return: returns 0
+ *
+ * 1. We declare a variable called digit and initialize it to 0.
+ * 2. We start a for loop that will run as long as digit is less than or equal to 9.
+ * 3. We print the value of digit.
+ * 4. We increment digit by 1.
+ * 5. If digit is equal to 9, we set it to a.
+ * 6. We start a for loop that will run as long as digit is less than or equal to f.
+ * 7. We print the value of digit.
+ * 8. We increment digit by 1.
+ * 9. We print a new line.
+ * 10. We return 0.
+ *
+ * Time Complexity: O(1)
  */
 int main(void)
 {

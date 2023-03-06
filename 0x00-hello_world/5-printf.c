90fe300d34fc0a0a2203f2d3c5a6a125544147fa
dennisnderitu254 <dknderitu@gmail.com>
2023-03-06 16:58:41 +0300

    adding code functionality and time complexity

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,6 +4,23 @@
  *
  *
  * Return: return 0 everytime
+ *
+ * FUNCtiONALITY *
+ *
+ * 1. The first line of the file is a preprocessor command, indicated by #.
+ * This line tells the preprocessor to include the contents of the stdio.h
+ * standard input/output header file (this file is provided for you by the compiler).
+ *
+ * 2. The next line contains the main function. All C programs must have a main function.
+ *
+ * 3. The main function is where the execution of your program begins.
+ *
+ * 4. The printf function is used to print output to the screen.
+ *
+ * 5. The return 0; statement is the "Exit status" of the program.
+ * In this case, the main function is saying it is going to return a 0.
+ *
+ * Time Complexity: O(1)
  */
 int main(void)
 {

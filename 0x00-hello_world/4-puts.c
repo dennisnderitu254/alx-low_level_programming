6cdec90e1c2c0843b24ec002993f7814946e992e
dennisnderitu254 <dknderitu@gmail.com>
2023-03-06 16:52:14 +0300

    adding code functionality and time complexity

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,6 +4,21 @@
  *
  * Return: Always  0 (success)
  *
+ * FUNCTIONALITY *
+ *
+ * 1. The first line of the program #include <stdio.h> is a preprocessor command,
+ * which tells a C compiler to include stdio.h file before going to actual compilation.
+ *
+ * 2. The next line int main(void) is the main function where the program execution begins.
+ *
+ * 3. The next line puts("\"Programming is like building a multilingual puzzle");
+ * causes the message Programming is like building a multilingual puzzle
+ * to be displayed on the screen.
+ *
+ * 4.The next line return 0; terminates main( ) function and causes it to
+ * return the value 0 to the calling process.
+ *
+ * Time Complexity: O(1)
  */
 int main(void)
 {

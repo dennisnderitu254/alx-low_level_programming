2867e5ab8ad4181b33aeede76c73112ea9cd28dc
dennisnderitu254 <dknderitu@gmail.com>
2023-03-06 19:28:43 +0300

    adding code functionality and time complexity

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,6 +4,16 @@
  * main - Prints all single digits of base 10 followed by new line
  *
  * Return: return 0
+ *
+ * 1. Declare a variable called digit of type int.
+ * 2. Initialize the variable digit to the character ‘0’.
+ * 3. While the variable digit is less than or equal to the character ‘9’, do the following:
+	a. Print the value of the variable digit.
+	b. Increment the variable digit by 1.
+ * 4. Print a newline character.
+ * 5. Return 0.
+ *
+ * Time Complexity: O(1)
  */
 int main(void)
 {

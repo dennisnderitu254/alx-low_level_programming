4496fa468dc867ced6d9306e0348019de33c2044
dennisnderitu254 <dknderitu@gmail.com>
2023-03-06 17:05:40 +0300

    adding code functionality and time complexity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,25 @@
  *
  *
  * return: return 1
+ *
+ * FUNCTIONALITY *
+ *
+ * 1. The first line of the code is a comment.
+ * Comments are ignored by the compiler.
+ *
+ * 2. The next line includes the standard input/output library.
+ * This library contains functions such as printf, scanf, etc.
+ *
+ * 3. The next line is the main function. Every C program must have a main function.
+ *
+ * 4. The next line is a function call to printf.
+ * printf is a function that prints a string to the standard output.
+ *
+ * 5. The next line is a return statement. Every function in C must return a value.
+ *
+ * 6. The last line is the end of the main function.
+ *
+ * Time Complexity: O(1)
  */
 int main(void)
 {

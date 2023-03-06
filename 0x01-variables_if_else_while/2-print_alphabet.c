192c5e224b6bf3f51bdf9804536447bdce866022
dennisnderitu254 <dknderitu@gmail.com>
2023-03-06 17:22:27 +0300

    adding code functionality and time complexity

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,7 +5,19 @@
  *
  * Return: Always 0 (success)
  *
+ * FUNCTIONALITY *
+ *
+ * 1. The for loop is used to iterate through the alphabet.
+ *
+ * 2. The tolower() function is used to convert the letter to lowercase.
+ *
+ * 3. The putchar() function is used to print the letter.
+ *
+ * 4. The putchar() function is used to print a new line.
+ *
+ * Time Complexity: O(n)
  */
+
 int main(void)
 {
 	int letter;

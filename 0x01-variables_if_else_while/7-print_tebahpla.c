342a6bc8af84e9952d5f358724095d0538565723
dennisnderitu254 <dknderitu@gmail.com>
2023-03-06 19:32:12 +0300

    adding code functionality and time complexity

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -4,6 +4,16 @@
  * main - Program to print alphabet letters in reverse followed by new line
  *
  * Return: return 0
+ *
+ * 1. Declare a variable called letter of type int.
+ * 2. Start a for loop that will run as long as letter is greater than or equal to the ASCII value of ‘a’.
+ * 3. Inside the for loop, print the value of letter.
+ * 4. Decrement the value of letter by 1.
+ * 5. End the for loop.
+ * 6. Print a new line.
+ * 7. Return 0.
+ *
+ * Time Complexity: O(n)
  */
 int main(void)
 {

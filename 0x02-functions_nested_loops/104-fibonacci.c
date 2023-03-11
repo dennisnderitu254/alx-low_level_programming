0c5ffd07c0166d0c4b477ca700048cf03f2851e2
dennisnderitu254 <dknderitu@gmail.com>
2023-03-11 11:36:45 +0300

    Adding Code Functionality and Time Complexity

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -4,10 +4,13 @@
  * starting with 1 and 2
  * followed by a new line
  * Return: Always 0 (Success)
+ *
+ * FUNCTIONALITY *
+ *
+ * It prints the first 100 Fibonacci numbers.
+ *
  */
-/*
-It prints the first 100 Fibonacci numbers.
-*/
+
 int main(void)
 {
 	unsigned long int i, j, k, j1, j2, k1, k2;

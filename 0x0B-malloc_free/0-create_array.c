6145076de75a51660e88260aa59bc9b8e4c84dfa
dennisnderitu254 <dknderitu@gmail.com>
2023-03-20 22:02:56 +0300

    Adding code functionality and Time Complexity

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,14 +1,23 @@
 #include "main.h"
 
 /**
-* create_array - creates an array of chars, and
-* initializes it with a specific char.
-*@size: Size of the array
-*@c: Character to insert
-*Return: NULL if size is zero or if it fails,
-*pointer to array if everything is normal.
-*/
+ * create_array - creates an array of chars, and
+ * initializes it with a specific char.
+ *@size: Size of the array
+ *@c: Character to insert
+ *Return: NULL if size is zero or if it fails,
+ *pointer to array if everything is normal.
 
+ *FUNCTIONALITY *
+
+ * 1. First, we check if the size is 0. If it is, we return NULL.
+ * 2. Then, we allocate memory for the array.
+ * 3. If the allocation fails, we return NULL.
+ * 4. If the allocation succeeds, we loop through the array and set each element to the value of c.
+ * 5. Finally, we return the array.
+ * Time Complexity: O(n)
+
+*/
 char *create_array(unsigned int size, char c)
 {
 	char *array;

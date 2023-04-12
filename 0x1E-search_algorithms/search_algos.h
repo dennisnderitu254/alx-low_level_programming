79a8700f94d360f13bb4303ea44329d90461630e
dennisnderitu254 <dknderitu@gmail.com>
2023-04-12 18:04:17 +0300

    function that searches for a value in a sorted array of integers using the Exponential search algorithm

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -45,5 +45,7 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int _binary_search(int *array, size_t left, size_t right, int value);
 
 #endif
